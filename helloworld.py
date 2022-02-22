f454aadf45c3a2334e2e31e6f43e805a745ba574
宇创小涵 <89262123+yuhan2680@users.noreply.github.com>
2022-02-22 16:27:35 +0800

    程序

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+
+from manimlib import *
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(BLUE, opacity=0.5)
+        circle.set_stroke(BLUE_E, width=4)
+
+        self.add(circle)
